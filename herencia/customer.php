f97a5c50a3a500cdcb466842369b8806b5db4df7
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-10-17 15:27:34 -0500

    herencias

--- herencia/customer.php
+++ herencia/customer.php
@@ -6,13 +6,36 @@ class customer extends people
     private $user;
     private $password;
     private $arrProducts;
-    public function __construct()
+    public function __construct(string $user, string $password, string $name, string $lastName, string $dni, string $email, string $typeSex)
     {
-        $this->setName("Yeison");
-        $this->setLastName("Carhuapoma Dett");
-        $this->setDni("71846271");
-        $this->setEmail("yeison@gmail.com");
-        $this->setTypeSex("Masculino");
-        echo  $this->getInfoPeople();
+        $this->setName($name);
+        $this->setLastName($lastName);
+        $this->setDni($dni);
+        $this->setEmail($email);
+        $this->setTypeSex($typeSex);
+        $this->user = $user;
+        $this->password = $password;
+    }
+    public function setProduccts(array $arrProducts)
+    {
+        $this->arrProducts = $arrProducts;
+    }
+
+    public function infoOrderProductos()
+    {
+        if ($this->user != "admin" || $this->password != "123456") {
+            echo "No tiene acceso";
+            die();
+        }
+        echo $this->getInfoPeople();
+        echo "<hr><br>";
+        $total = 0;
+        foreach ($this->arrProducts as $key => $value) {
+            $total += ($value["cant"] * $value["precio"]);
+            echo "Producto: " . $value["producto"] . " Cantidad: "
+                . $value["cant"] . " Precio: " . $value["precio"] .
+                "Sub Total: " . ($value["cant"] * $value["precio"]) . "<br>";
+        }
+        echo "Total: s/. " . $total;
     }
 }
--- herencia/index.php
+++ herencia/index.php
@@ -1,4 +1,25 @@
 <?php
 require_once 'customer.php';
 //creando un objeto de la clase customer
-$customer = new customer();
+$customer = new customer("admin", "123456", "Yeison Danner", "Sanchez", "741258963", "yeison@gmail.com", "Masculino");
+$customer->setProduccts(
+    [
+        array(
+            "cant" => 2,
+            "producto" => "leche",
+            "precio" => 2.5,
+        ),
+        array(
+            "cant" => 4,
+            "producto" => "Galletas",
+            "precio" => 0.5,
+        ),
+        array(
+            "cant" => 12,
+            "producto" => "Ceverzas",
+            "precio" => 6,
+        )
+    ]
+);
+
+$customer->infoOrderProductos();
